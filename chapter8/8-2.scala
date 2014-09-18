c6b8bdeb3aeb479b55a52f209dfe130bedd50c59
amico <amico8amico@gmail.com>
2014-09-18 18:24:45 +0900

    chapter 8

--- chapter8/8-2.scala
+++ chapter8/8-2.scala
@@ -13,7 +13,7 @@ object LongLines {
   def processFile(filename: String, width: Int) {
     def processLine(line: String) {
       if (line.length > width)
-        println(filename + ": " + line.trim)
+        println(filename + " : " + line.trim)
     }
     val source = Source.fromFile(filename)
     for (line <- source.getLines())
